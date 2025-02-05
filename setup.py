64e4385ac3989a3efcba28d452d146448917e10e
Michael Benea <michaelanjelo61@gmail.com>
2025-02-06 01:48:29 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Saint Michael',
+    author_email= 'm.colegalaxy@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
